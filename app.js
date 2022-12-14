62e4c14f9fbf2bd7acd3302836c3c2cb82e4c8be
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-12-13 19:53:15 -0500

    feat: add compression

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
     const express = require('express')
+    const compression = require('compression')
 
     const app = express()
+    app.use(compression())
+
     const server = app.listen(8080, () => console.log('Server Up'))
 
     app.get('/', (req, res) => {
